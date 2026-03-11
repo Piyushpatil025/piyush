b2bff1c9f020dc191fcbcd8fe14a7e843db269a7
Piyushpatil025 <piyushborse1001@gmail.com>
2026-03-11 13:21:36 +0530

    piyush
    jk

---
+++ Fibonacci Series.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{	
+	int n, first = 0, second = 1, next,i;
+	printf("Enter the number of terms: ");
+	scanf("%d", &n);
+	printf("Fibonacci Series: ");
+	{
+		if (i <= 1)
+	   {
+					next = i;		
+		}
+else 
+		{ 
+						next = first + second;
+						first = second;
+						second = next;
+		}
+			printf("%d ", next);
+}
+	printf("\n");
+	return 0;
+	
+}
+	
+
